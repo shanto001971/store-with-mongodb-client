ce07dea62b697d7abaf9cc9375e283206865cfdd
Shanto Islam <rajs45693@gmail.com>
2023-05-07 01:30:30 +0600

    add delete btn for db

--- src/Components/Users.jsx
+++ src/Components/Users.jsx
@@ -1,25 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 
 const Users = () => {
     const data = useLoaderData()
+    const [user, setUser] = useState(data)
     console.log(data)
 
 
-    const handelDelite =(_id)=>{
+    const handelDelite = (_id) => {
         console.log(_id)
-        fetch(`http://localhost:5000/user/${_id}`,{
-            method:'DELETE',
+        fetch(`http://localhost:5000/user/${_id}`, {
+            method: 'DELETE',
         })
-        .then(res=>res.json())
-        .then(data=>{
-            console.log(data)
-        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if (data.deletedCount > 0) {
+                    alert('delete sucssesfully');
+                    const remaningUser = user.filter(singleUser => singleUser._id !== _id);
+                    setUser(remaningUser);
+                }
+            })
     }
     return (
         <div>
             {
-                data.map(singleData => <h1 key={singleData._id}>{singleData.name}  {singleData.email} <button onClick={()=> handelDelite(singleData._id)}>x</button></h1>)
+                user.map(singleData => <h1 key={singleData._id}>{singleData.name}  {singleData.email} <button onClick={() => handelDelite(singleData._id)}>x</button></h1>)
             }
             <h1></h1>
         </div>
